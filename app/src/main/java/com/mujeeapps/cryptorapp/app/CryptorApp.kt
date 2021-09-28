dba0ffa512df9bf6a1d40bff06918e341c2790fc
Ahamed <a.mujee@dishserve.com>
2021-09-28 10:37:44 +0530

    Package refactoring

--- app/src/main/java/com/mujeeapps/cryptorapp/app/CryptorApp.kt
+++ app/src/main/java/com/mujeeapps/cryptorapp/app/CryptorApp.kt
@@ -1,9 +1,8 @@
-package com.mujeeapps.cryptorapp
+package com.mujeeapps.cryptorapp.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CoinApplication : Application() {
-
+class CryptorApp : Application() {
 }
